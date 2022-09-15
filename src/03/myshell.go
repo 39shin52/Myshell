596cbb61d0917243cca878b8c9972baa4baed372
Shingo Itoh <g2122006@fun.ac.jp>
2022-09-15 19:33:55 +0900

    [add] 課題1完成

--- src/03/myshell.go
+++ src/03/myshell.go
@@ -12,9 +12,44 @@ import (
 	"syscall"
 )
 
+func doCommand(text string) int {
+	command := strings.Split(strings.TrimSpace(text), " ")
+
+	cpath, err := exec.LookPath(command[0])
+	if err != nil {
+		log.Printf("%s not found in $PATH.", text)
+		return 1 // ここでreturnすることでforcexecにcpathがいかないのでpanicまで到達しない
+	}
+
+	args := command[0:]
+	attr := syscall.ProcAttr{
+		Files: []uintptr{0, 1, 2},
+	}
+
+	pid, err := syscall.ForkExec(cpath, args, &attr)
+	if err != nil {
+		panic(err)
+	}
+
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		panic(err)
+	}
+
+	status, err := proc.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	if !status.Success() {
+		fmt.Println(status.String())
+	}
+
+	return status.ExitCode()
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-
 	count := 0
 	fmt.Print("./Myshell[00]> ")
 
@@ -26,38 +61,9 @@ func main() {
 
 		if len(scanner.Text()) > 0 {
 			count++
-
-			command := strings.Split(strings.TrimSpace(scanner.Text()), " ")
-
-			cpath, err := exec.LookPath(command[0])
-			if err != nil {
-				log.Fatalf("%s not found in $PATH.", scanner.Text())
-			}
-
-			args := command[0:]
-			attr := syscall.ProcAttr{
-				Files: []uintptr{0, 1, 2},
-			}
-
-			pid, err := syscall.ForkExec(cpath, args, &attr)
-			if err != nil {
-				panic(err)
-			}
-
-			proc, err := os.FindProcess(pid)
-			if err != nil {
-				panic(err)
-			}
-
-			status, err := proc.Wait()
-			if err != nil {
-				panic(err)
-			}
-
-			if !status.Success() {
-				fmt.Println(status.String())
-			}
+			doCommand(scanner.Text())
 		}
+
 		fmt.Printf("./Myshell[%02d]> ", count)
 	}
 }
